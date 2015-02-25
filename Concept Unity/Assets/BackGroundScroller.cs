0aca8de04f040871d891f5daef439309f13919fe
Ronnie E <rel-alam@hotmail.com>
2015-02-25 15:20:59 +1100

    + Scrolling background test in unity + testing out unity

---
+++ Concept Unity/Assets/BackGroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScroller : MonoBehaviour 
+{
+
+	public float scrollSpeed;
+
+	private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		float newPos = Mathf.Repeat (Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(0, newPos);
+       // renderer.sharedMaterial.SetTextureOffset("
+        transform.position = startPos + Vector3.forward * newPos;
+	
+	}
+}
---
+++ Concept Unity/Assets/Scroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroller : MonoBehaviour {
+
+    public float scrollSpeed;
+
+    private Vector2 savedOffset;
+
+	// Use this for initialization
+	void Start () {
+
+        savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, savedOffset.y);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	
+	}
+
+    void OnDisable()
+    {
+
+    }
+}
